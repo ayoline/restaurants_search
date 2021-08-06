499727bc8ae986744355ee3231b45f8ce29feec8
ayoline <ayo@outlook.com.br>
2021-08-06 17:05:19 -0300

    altera várias configs da interface

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import TextField, { Input } from "@material/react-text-field/dist/index";
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from "./styles";
+import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import MaterialIcon from '@material/react-material-icon';
+import { Card } from "../../components";
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+    };
+
+    return (
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="Logo do restaurante" />
+                    <TextField
+                        label="Pesquisar"
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}>
+                        <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                    </TextField>
+                    <CarouselTitle>Na sua Área</CarouselTitle>
+                    <Slider {...settings}>
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                    </Slider>
+                </Search>
+            </Container>
+            <Map />
+        </Wrapper>
+    );
+};
+
+export default Home;
